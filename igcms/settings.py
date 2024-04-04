75aa2569a4bd1b7d11abd98c6ff1703460b394a3
Antonietta Albiso <09tonith@gmail.com>
2024-04-04 08:01:25 +0800

    create incidents model

--- igcms/settings.py
+++ igcms/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'incidents.apps.IncidentsConfig',
     'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
